516e67f61462aa6a32bb179d89e7b8fb562a959c
flopp <fpigorsch@gmail.com>
2017-12-08 18:55:40 +0100

    refactoring

--- 08/c++/main.cpp
+++ 08/c++/main.cpp
@@ -4,40 +4,26 @@
 #include <string>
 
 bool cmp(int v1, const std::string& c, int v2) {
-    if (c == "==") {
-        return (v1 == v2);
-    } else if (c == "!=") {
-        return (v1 != v2);
-    } else if (c == "<") {
-        return (v1 < v2);
-    } else if (c == "<=") {
-        return (v1 <= v2);
-    } else if (c == ">") {
-        return (v1 > v2);
-    } else if (c == ">=") {
-        return (v1 >= v2);
-    } else {
-        std::cout << "bad cmp '" << c << "'" << std::endl;
-        return false;
-    }
+    if      (c == "==") { return (v1 == v2); }
+    else if (c == "!=") { return (v1 != v2); }
+    else if (c == "<" ) { return (v1 <  v2); }
+    else if (c == "<=") { return (v1 <= v2); }
+    else if (c == ">" ) { return (v1 >  v2); }
+    else if (c == ">=") { return (v1 >= v2); }
+
+    return false;
 }
 
 int main() {
     auto registers = std::map<std::string, int>{};
-    auto re_instruction_inc = std::regex{"^\\s*([a-z]+)\\s+inc\\s+(-?\\d+)\\s+if\\s+([a-z]+)\\s+([<>=!]{1,2})\\s+(-?\\d+)\\s*$"};
-    auto re_instruction_dec = std::regex{"^\\s*([a-z]+)\\s+dec\\s+(-?\\d+)\\s+if\\s+([a-z]+)\\s+([<>=!]{1,2})\\s+(-?\\d+)\\s*$"};
+    auto re_instruction = std::regex{"^\\s*([a-z]+)\\s+(inc|dec)\\s+(-?\\d+)\\s+if\\s+([a-z]+)\\s+([<>=!]{1,2})\\s+(-?\\d+)\\s*$"};
     
     auto max = int{0};
     for (std::string line; std::getline(std::cin, line); /**/) {
         std::smatch m;
-        if (std::regex_match(line, m, re_instruction_inc)) {
-            if (cmp(registers[m[3]], m[4], std::stoi(m[5]))) {
-                registers[m[1]] += std::stoi(m[2]);
-                max = std::max(max, registers[m[1]]);
-            }
-        } else if (std::regex_match(line, m, re_instruction_dec)) {
-            if (cmp(registers[m[3]], m[4], std::stoi(m[5]))) {
-                registers[m[1]] -= std::stoi(m[2]);
+        if (std::regex_match(line, m, re_instruction)) {
+            if (cmp(registers[m[4]], m[5], std::stoi(m[6]))) {
+                registers[m[1]] += ((m[2] == "inc") ? +1 : -1) * std::stoi(m[3]);
                 max = std::max(max, registers[m[1]]);
             }
         }
