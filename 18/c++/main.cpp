2da2a9d5d4f522ee309f95cef0344e09580a3221
Florian Pigorsch <florian@concept.de>
2017-12-18 09:50:04 +0100

    finish day 18

--- 18/c++/main.cpp
+++ 18/c++/main.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <deque>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -17,18 +18,92 @@ enum class Type {
 struct Instruction {
     Type type;
     bool const_value;
-    int target;
-    int value;
+    int64_t target;
+    int64_t value;
     std::string line;
+};
+
+struct Process {
+    Process(int64_t id, const std::vector<Instruction>& instructions) :
+        _instructions(instructions),
+        _mem(26, 0) {
+        _mem['p' - 'a'] = id;
+    }
+    
+    bool compute(Process* other) {
+        if (_pc < 0 || _pc >= (int64_t)_instructions.size()) {
+            return false;
+        }
+        
+        const auto& inst = _instructions[_pc];
+        switch (inst.type) {
+            case Type::SET:
+                _mem[inst.target] = inst.const_value ? inst.value : _mem[inst.value];
+                ++_pc;
+                break;
+            case Type::ADD:
+                _mem[inst.target] += inst.const_value ? inst.value : _mem[inst.value];
+                ++_pc;
+                break;
+            case Type::MUL:
+                _mem[inst.target] *= inst.const_value ? inst.value : _mem[inst.value];
+                ++_pc;
+                break;
+            case Type::MOD:
+                _mem[inst.target] %= (inst.const_value ? inst.value : _mem[inst.value]);
+                ++_pc;
+                break;
+            case Type::SND:
+#ifdef PART1
+                _send = _mem[inst.target];
+#else
+                ++_send;
+                other->_msg.push_back(_mem[inst.target]);
+#endif
+                ++_pc;
+                break;
+            case Type::RCV:
+#ifdef PART1
+                if (_send != 0) {
+                    return false;
+                }
+#else
+                if (_msg.empty()) {
+                    return false;
+                }
+                _mem[inst.target] = _msg.front();
+                _msg.pop_front();
+#endif
+                 ++_pc;
+                break;
+            case Type::JGZ:
+                if (_mem[inst.target] > 0) {
+                    _pc += inst.const_value ? inst.value : _mem[inst.value];
+                } else {
+                    ++_pc;
+                }
+                break;
+            case Type::JGZI:
+                if (inst.target > 0) {
+                    _pc += inst.const_value ? inst.value : _mem[inst.value];
+                } else {
+                    ++_pc;
+                }
+                break;
+        }
+        return true;
+    }
+    
+    std::vector<Instruction> _instructions;
+    std::vector<int64_t> _mem;
+    std::deque<int64_t> _msg;
+    int _pc{0};
+    int _send{0};
 };
 
 int main() {
     std::vector<Instruction> instructions;
-    std::vector<int> mem(26, 0);
-    int pc = 0;
-    
     for (std::string line; std::getline(std::cin, line); /**/) {
-        std::cout << line << std::endl;
         Instruction inst;
         inst.line = line;
         std::string s;
@@ -51,7 +126,6 @@ int main() {
             inst.target = (line[4] - '0');
         }
         
-        std::cout << "target = " << inst.target << std::endl;
         inst.value = 0;
         inst.const_value = true;
         if (line.size() >= 7) {
@@ -62,70 +136,32 @@ int main() {
                 inst.value = std::stoi(line.substr(6));
                 inst.const_value = true;
             }
-            std::cout << "value = " << inst.value << std::endl;
         }
         
         instructions.push_back(inst);
     }
     
-    int snd = 0;
-    while (true) {
-        const auto& inst = instructions[pc];
-        std::cout << pc << ": " << inst.line << std::endl;
-        switch (inst.type) {
-            case Type::SET:
-                mem[inst.target] = inst.const_value ? inst.value : mem[inst.value];
-                std::cout << "-> " << mem[inst.target] << std::endl;
-                ++pc;
-                break;
-            case Type::ADD:
-                mem[inst.target] += inst.const_value ? inst.value : mem[inst.value];
-                std::cout << "-> " << mem[inst.target] << std::endl;
-                ++pc;
-                break;
-            case Type::MUL:
-                mem[inst.target] *= inst.const_value ? inst.value : mem[inst.value];
-                std::cout << "-> " << mem[inst.target] << std::endl;
-                ++pc;
-                break;
-            case Type::MOD:
-                if (inst.const_value) {
-                    std::cout << mem[inst.target] << " % " << inst.value << std::endl;
-                } else {
-                    std::cout << mem[inst.target] << " % " << mem[inst.value] << " (mem[" << inst.value << "])" << std::endl;
-                }
-                mem[inst.target] %= (inst.const_value ? inst.value : mem[inst.value]);
-                std::cout << "-> " << mem[inst.target] << std::endl;
-                ++pc;
-                break;
-            case Type::SND:
-                snd = mem[inst.target];
-                std::cout << "-> " << snd << std::endl;
-                ++pc;
-                break;
-            case Type::RCV:
-                if (snd != 0) {
-                    std::cout << snd << std::endl;
-                    return 0;
-                }
-                ++pc;
-                break;
-            case Type::JGZ:
-                if (mem[inst.target] > 0) {
-                    pc += inst.const_value ? inst.value : mem[inst.value];
-                } else {
-                    ++pc;
-                }
-                break;
-            case Type::JGZI:
-                if (inst.target > 0) {
-                    pc += inst.const_value ? inst.value : mem[inst.value];
-                } else {
-                    ++pc;
-                }
-                break;
-        }
+    Process p0(0, instructions);
+#ifdef PART2
+    Process p1(1, instructions);
+#endif
+
+    bool r = true;
+    while (r) {
+#ifdef PART1
+        r = p0.compute(nullptr);
+#else
+        bool r0 = p0.compute(&p1);
+        bool r1 = p1.compute(&p0);
+        r = r0 || r1;
+#endif
     }
+
+#ifdef PART1    
+    std::cout << p0._send << std::endl;
+#else
+    std::cout << p1._send << std::endl;
+#endif
     
     return 0;
 }
